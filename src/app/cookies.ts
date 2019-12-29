325aaa4b2cc1f0d70ecc1ecf815b766e30037c95
Kirill.Kolomin <kirill.kolomin@sidenis.com>
2019-12-29 14:52:13 +0700

    Defined handling for the cookie action requests

--- src/app/cookies.ts
+++ src/app/cookies.ts
@@ -1,15 +1,23 @@
 import {Application, Request, Response} from "express";
+import {CookieAction} from "./interfaces/cookie-action";
+import {CookieActions} from "./enums/cookie-actions";
 
 export default setCookieRouterToApplication;
 
-const setCookie = (request: Request, response: Response): void => {
-    console.log(request);
+const doCookieAction = (request: Request, response: Response): void => {
+    const cookieAction: CookieAction = request.body;
 
-    response.send(true);
+    if(cookieAction.action === CookieActions.Delete) {
+        response.clearCookie(cookieAction.name);
+    } else {
+        response.cookie(cookieAction.name, cookieAction.value, cookieAction.options);
+    }
+
+    response.send('done');
 };
 
 function setCookieRouterToApplication(application: Application): void {
     application.route('/cookies')
-        .post(setCookie);
+        .post(doCookieAction);
 }
 
